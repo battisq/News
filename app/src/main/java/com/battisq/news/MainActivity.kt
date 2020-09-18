dd7beaff6c1741e6cc027e4354de0a4360622278
battisq <airget2000@gmail.com>
2020-09-18 16:41:47 +0700

    Added Splash Screen and Navigation Graph

--- app/src/main/java/com/battisq/news/MainActivity.kt
+++ app/src/main/java/com/battisq/news/MainActivity.kt
@@ -2,10 +2,31 @@ package com.battisq.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.battisq.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private var binding: ActivityMainBinding? = null
+    private val mBinging: ActivityMainBinding get() = binding!!
+    private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mToolbar = mBinging.toolbar
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
